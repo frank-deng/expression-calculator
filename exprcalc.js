0efc6b41eca912f8215a35da8eec544b8c5386d6
frank-deng <13382084679@163.com>
2017-03-05 23:21:58 +0800

    Add syntax check

--- exprcalc.js
+++ exprcalc.js
@@ -1,64 +1,119 @@
 /* Token types */
-var NUMBER = 1, LABEL = 2, OPER = 3;
-
-var lexTable = [
-	function(input) {
-		var match = /^0[0-7]+/g.exec(input);
-		if (match) {
-			return {type:NUMBER, len:match[0].length, content:parseInt(match[0], 8)};
-		}
-		match = /^0x[0-9A-Fa-f]+/g.exec(input);
-		if (match) {
-			return {type:NUMBER, len:match[0].length, content:Number(match[0])};
-		}
-		match = /^[0-9]+(\.[0-9]+)?([Ee][+\-][0-9]+)?/g.exec(input);
-		if (match) {
-			return {type:NUMBER, len:match[0].length, content:Number(match[0])};
-		}
-		return false;
-	},
-	function(input) {
-		var match = /^[A-Za-z_\u0080-\uffff][A-Za-z0-9_\-\u0080-\uffff]*/g.exec(input);
-		if (match) {
-			return {type:LABEL, len:match[0].length, content:match[0]};
-		}
-		return false;
-	},
-	function(input) {
-		var match = /^[+\-\*\/=\(\)]/g.exec(input);
-		if (match) {
-			return {type:OPER, len:match[0].length, content:match[0]};
-		}
-		return false;
-	},
-	function(input) {
-		var match = /^\s+/g.exec(input);
-		if (match) {
-			return {type:null, len:match[0].length};
-		}
-		return false;
-	},
-];
+var NUMBER = 1, VAR = 2, OPER = 3;
 function lexParser(input){
-	var result = [];
+	var lexTable = [
+		function(input) {
+			var match = /^0x[0-9A-Fa-f]+/g.exec(input);
+			if (match) {
+				return {type:NUMBER, content:match[0]};
+			}
+			match = /^[0-9]+(\.[0-9]+)?([Ee][+\-][0-9]+)?/g.exec(input);
+			if (match) {
+				return {type:NUMBER, content:match[0]};
+			}
+			return false;
+		},
+		function(input) {
+			var match = /^[A-Za-z_\u0080-\uffff][A-Za-z0-9_\-\u0080-\uffff]*/g.exec(input);
+			if (match) {
+				return {type:VAR, content:match[0]};
+			}
+			return false;
+		},
+		function(input) {
+			var match = /^[+\-\*\/=\(\)]/g.exec(input);
+			if (match) {
+				return {type:OPER, content:match[0]};
+			}
+			return false;
+		},
+		function(input) {
+			var match = /^\s+/g.exec(input);
+			if (match) {
+				return {type:null, content:match[0]};
+			}
+			return false;
+		},
+	];
+	var result = [], pos = 0, token_last = undefined, token;
 	while (input.length) {
-		var token = undefined;
 		for (var i = 0; i < lexTable.length; i++) {
 			token = lexTable[i](input);
 			if (token) {
 				if (token.type !== null) {
+					token.pos = pos;
 					result.push(token);
 				}
-				input = input.slice(token.len);
+				pos += token.content.length;
+				input = input.slice(token.content.length);
 				break;
 			}
 		}
 		if (!token) {
-			throw {error:"Unexpected Character", token:input[0]};
+			throw "Unexpected Character At Position " + pos;
+		} else if (null !== token.type){
+			if (token.type == OPER && token.content == '-') {
+				if (undefined == token_last) {
+					token.content = 'NEG';
+				} else if (token_last.type == OPER && token_last.content != ')') {
+					token.content = 'NEG';
+				}
+			}
+			token_last = token;
 		}
 	}
 	return result;
 }
+function getTokenType(token) {
+	if (token.type == NUMBER || token.type == VAR) {
+		return 'N';
+	} else if (token.type == OPER) {
+		if (token.content == '(') {
+			return '(';
+		} else if (token.content == ')') {
+			return ')';
+		} else if (operand[token.content].binocular) {
+			return 'B';
+		} else {
+			return 'S';
+		}
+	}
+	return null;
+}
+function syntaxCheck(tokens) {
+	var syntaxTable = {
+		'START' : ['N', 'S', '('],
+		'N' : ['B', ')', 'END'],
+		'S' : ['N', '(', 'S'],
+		'B' : ['N', '(', 'S'],
+		'(' : ['S', 'N', '('],
+		')' : ['END', 'B'],
+	}
+	var t_last = 'START', t, stack_bracket = new Array();
+	for (var i = 0; i < tokens.length; i++) {
+		var t = getTokenType(tokens[i]);
+		if (syntaxTable[t_last].indexOf(t) < 0) {
+			return tokens[i];
+		}
+		if ('(' == t) {
+			stack_bracket.push(tokens[i]);
+		} else if (')' == t) {
+			if (stack_bracket.length == 0) {
+				return tokens[i];
+			} else {
+				stack_bracket.pop();
+			}
+		}
+		t_last = t;
+	}
+	if (syntaxTable[t_last].indexOf('END') < 0) {
+		return tokens[i];
+	} else if (stack_bracket.length > 0) {
+		return stack_bracket[stack_bracket.length - 1];
+	}
+	return null;
+}
+
 var operand = {
 	'+': {
 		binocular: true,
@@ -98,70 +153,42 @@ var operand = {
 			return -a;
 		}
 	},
-}
+};
 function rplParse(tokens) {
 	var stack_main = new Array(), stack_oper = new Array();
-	var token_pop, verification = 0;
+	var token_pop;
 
 	for (var i = 0; i < tokens.length; i++) {
 		var token = tokens[i];
-		if (token.type == NUMBER || token.type == LABEL) {
+		if (token.type == NUMBER) { 
+			token.content = Number(token.content);
 			stack_main.push(token);
-			verification++;
+		} else if (token.type == VAR) {
+			stack_main.push(token);
 		} else if (token.type == OPER) {
-			//Pick out negative operation
-			if (token.content == '-'
-					&& (i == 0 || (tokens[i-1].type == OPER && tokens[i-1].content != ')'))) {
-				token.content = tokens[i].content = 'NEG';
-			}
-
 			if (stack_oper.length == 0 || token.content == '(' || stack_oper[stack_oper.length-1].content == '(') {
 				stack_oper.push(token);
 			} else if (token.content == ')') {
 				while (token_pop = stack_oper.pop()) {
-					if (stack_oper.length == 0) {
-						throw {error:"Unmatched Bracket", token:token_pop};
-					} else if (token_pop.content == '(') {
+					if (token_pop.content == '(') {
 						break;
-					}
-
-					if (operand[token_pop.content].binocular) {
-						if (verification <= 1) {
-							throw {error:"Syntax Error"};
-						}
-						verification--;
 					}
 					stack_main.push(token_pop);
 				}
 			} else {
 				if (undefined === operand[token.content]) {
-					throw {error:"Invalid Operand", token:token};
+					throw "Syntax Error";
 				}
 				while (stack_oper.length > 0
 						&& stack_oper[stack_oper.length-1].content != '('
 						&& operand[stack_oper[stack_oper.length-1].content].priority >= operand[token.content].priority) {
-					token_pop = stack_oper.pop();
-					if (operand[token_pop.content].binocular) {
-						if (verification <= 1) {
-							throw {error:"Syntax Error"};
-						}
-						verification--;
-					}
-					stack_main.push(token_pop);
+					stack_main.push(stack_oper.pop());
 				}
 				stack_oper.push(token);
 			}
 		}
 	}
 	while (token_pop = stack_oper.pop()) {
-		if (token_pop.content == '(') {
-			throw {error:"Unmatched Bracket", token:token_pop};
-		} else if (operand[token_pop.content].binocular) {
-			if (verification <= 1) {
-				throw {error:"Syntax Error"};
-			}
-			verification--;
-		}
 		stack_main.push(token_pop);
 	}
 	return stack_main;
@@ -172,7 +199,7 @@ function rplCalc(tokens, vars) {
 		var token = tokens[i];
 		if (token.type == NUMBER) {
 			stack.push(token.content);
-		} else if (token.type == LABEL) {
+		} else if (token.type == VAR) {
 			if (undefined === vars[token.content]) {
 				stack.push(0);
 			} else {
@@ -181,7 +208,7 @@ function rplCalc(tokens, vars) {
 		} else if (token.type == OPER){
 			//Calculate by opers
 			var operand_data = operand[token.content];
-			if (undefined !== operand_data) {
+			if (undefined === operand_data) {
 				throw "Calculation Error";
 			} else if (operand_data.binocular) {
 				if (stack.length <= 1) {
@@ -203,23 +230,22 @@ function rplCalc(tokens, vars) {
 	return stack[0];
 }
 
-//var expr = '3+(-3.1+4)*5+a';
-var util = require('util');
-var expr = '3+-(-3.1-4)*5';
-try {
+var exprAll = [
+	"- 3+ - (-3.1-4)*5/-10",
+	"3+-(-3.1-4)*5 10-",
+	"3-((-4)",
+	"3-(-4))",
+];
+for (var i = 0; i < exprAll.length; i++) {
+	var expr = exprAll[i];
+	console.log('------');
 	console.log(expr);
-	console.log(rplParse(lexParser(expr)));
-	console.log(rplCalc(rplParse(lexParser(expr))));
-} catch(e) {
-	console.log(e);
+	var syntaxError = syntaxCheck(lexParser(expr));
+	if (syntaxError) {
+		console.log('Syntax Error: ', syntaxError);
+	} else {
+		console.log(rplCalc(rplParse(lexParser(expr))));
+		console.log(eval(expr));
+	}
 }
-console.log(eval(expr));
-/*
-try {
-	console.log(rplParse(lexParser('3++4')));
-} catch(e) {
-	console.log(e);
-}
-*/
-//console.log(eval(expr));
 
