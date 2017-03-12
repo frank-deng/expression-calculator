7f02d06cb8db247f13df2e594a43452558fa059e
frank-deng <13382084679@163.com>
2017-03-12 22:02:51 +0800

    Add qunit-based test cases

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,41 +1,185 @@
-var Calc = require('./exprcalc');
+var Calc = require('./exprcalc.min');
 var exprGen = require('./exprgen');
-
-var exprAll = [
-	"- 3+ - (-3.1-4)*5/-10",
-	"3+-(-3.1-4)*5 10-",
-	"3-((-4))",
-	"3-((-4))+4",
-	"3-((-4)",
-	"3-(-4))",
-];
-var calc = new Calc();
-for (var i = 0; i < exprAll.length; i++) {
-	var expr = exprAll[i];
-	console.log('>>> '+expr);
-	try {
-		console.log(calc.compile(expr).calc());
-		console.log(eval(expr));
-	} catch(e) {
-		//console.log('Error: ', e);
-		console.log(e instanceof Calc.SyntaxError);
-	}
-}
-
-var count = 100;
-while (count--){
-	var expr = exprGen(20);
-	try {
-		var r0 = calc.compile(expr).calc();
-		var r1 = eval(expr);
-		if (r0 != r1) {
-			console.log('Error', expr, r0, r1);
-		} else {
-			console.log(expr, r0);
+QUnit.test("Basic Test", function(assert) {
+	assert.strictEqual((new Calc()).compile('1+1').calc(), 2);
+	assert.strictEqual((new Calc()).compile('- 3+ - (-3.1-4)*5/-10').calc(), -6.55);
+	assert.strictEqual((new Calc()).compile('3-((-4))').calc(), 7);
+	assert.strictEqual((new Calc()).compile('3-((-4))+4').calc(), 11);
+});
+QUnit.test("Compile Multiple Exprs", function(assert) {
+	var calc = new Calc();
+	assert.strictEqual(calc.compile('1+1').calc(), 2);
+	assert.strictEqual(calc.compile('- 3+ - (-3.1-4)*5/-10').calc(), -6.55);
+	assert.strictEqual(calc.compile('3-((-4))').calc(), 7);
+	assert.strictEqual(calc.compile('3-((-4))+4').calc(), 11);
+});
+QUnit.test("RPN Output", function(assert) {
+	var calc = new Calc();
+	assert.deepEqual(calc.compile('1+1').getRPN(), [
+		{type:Calc.TOKEN_NUM, value:1},
+		{type:Calc.TOKEN_NUM, value:1},
+		{type:Calc.TOKEN_OPER, value:'+'},
+	]);
+	assert.deepEqual(calc.compile('- 3+ - (-3.1-4)*5/-10').getRPN(), [
+		{type:Calc.TOKEN_NUM, value:3},
+		{type:Calc.TOKEN_OPER, value:'NEG'},
+		{type:Calc.TOKEN_NUM, value:3.1},
+		{type:Calc.TOKEN_OPER, value:'NEG'},
+		{type:Calc.TOKEN_NUM, value:4},
+		{type:Calc.TOKEN_OPER, value:'-'},
+		{type:Calc.TOKEN_OPER, value:'NEG'},
+		{type:Calc.TOKEN_NUM, value:5},
+		{type:Calc.TOKEN_OPER, value:'*'},
+		{type:Calc.TOKEN_NUM, value:10},
+		{type:Calc.TOKEN_OPER, value:'NEG'},
+		{type:Calc.TOKEN_OPER, value:'/'},
+		{type:Calc.TOKEN_OPER, value:'+'},
+	]);
+	assert.deepEqual(calc.compile('3-((-4))').getRPN(), [
+		{type:Calc.TOKEN_NUM, value:3},
+		{type:Calc.TOKEN_NUM, value:4},
+		{type:Calc.TOKEN_OPER, value:'NEG'},
+		{type:Calc.TOKEN_OPER, value:'-'},
+	]);
+	assert.deepEqual(calc.compile('3-((-4))+4').getRPN(), [
+		{type:Calc.TOKEN_NUM, value:3},
+		{type:Calc.TOKEN_NUM, value:4},
+		{type:Calc.TOKEN_OPER, value:'NEG'},
+		{type:Calc.TOKEN_OPER, value:'-'},
+		{type:Calc.TOKEN_NUM, value:4},
+		{type:Calc.TOKEN_OPER, value:'+'},
+	]);
+});
+QUnit.test("Run Without Expressions", function(assert) {
+	var calc = new Calc();
+	assert.strictEqual(calc.calc(), false);
+});
+QUnit.test("Malformed Expressions", function(assert) {
+	var calc = new Calc();
+	assert.throws(
+		function(){
+			calc.compile('3+#10');
+		},
+		function(e){
+			return (
+				(e instanceof Calc.SyntaxError)
+				&& e.message === 'Unexpected Character'
+				&& e.token === 2
+			);
 		}
-	} catch(e) {
-		console.log(e);
-		console.log(e.token);
+	);
+	assert.throws(
+		function(){
+			calc.compile('3--4');
+		},
+		function(e){
+			return (
+				(e instanceof Calc.SyntaxError)
+				&& e.message === 'Unexpected Character'
+				&& e.token === 1
+			);
+		}
+	);
+	assert.throws(
+		function(){
+			calc.compile('3+-(-3.1-4)*5 10-');
+		},
+		function(e){
+			return (
+				(e instanceof Calc.SyntaxError)
+				&& e.message === 'Unexpected Token'
+				&& e.token.type === Calc.TOKEN_NUM
+				&& e.token.value === '10'
+				&& e.token.pos === 14
+			);
+		}
+	);
+	assert.throws(
+		function(){
+			calc.compile('3-((-4)');
+		},
+		function(e){
+			return (
+				(e instanceof Calc.SyntaxError)
+				&& e.message === 'Unmatched Bracket'
+				&& e.token.type === Calc.TOKEN_OPER
+				&& e.token.value === '('
+				&& e.token.pos === 2
+			);
+		}
+	);
+	assert.throws(
+		function(){
+			calc.compile('3-(-4))');
+		},
+		function(e){
+			return (
+				(e instanceof Calc.SyntaxError)
+				&& e.message === 'Unmatched Bracket'
+				&& e.token.type === Calc.TOKEN_OPER
+				&& e.token.value === ')'
+				&& e.token.pos === 6
+			);
+		}
+	);
+	assert.strictEqual(calc.calc(), false);
+});
+QUnit.test("RPN Input", function(assert) {
+	var rpn, calc = new Calc();
+	rpn = [
+		{type:Calc.TOKEN_NUM, value:1},
+		{type:Calc.TOKEN_NUM, value:1},
+		{type:Calc.TOKEN_OPER, value:'+'},
+	];
+	assert.deepEqual(calc.setRPN(rpn).getRPN(), rpn);
+	assert.strictEqual(calc.setRPN(rpn).calc(), 2);
+
+	rpn = [
+		{type:Calc.TOKEN_NUM, value:3},
+		{type:Calc.TOKEN_OPER, value:'NEG'},
+		{type:Calc.TOKEN_NUM, value:3.1},
+		{type:Calc.TOKEN_OPER, value:'NEG'},
+		{type:Calc.TOKEN_NUM, value:4},
+		{type:Calc.TOKEN_OPER, value:'-'},
+		{type:Calc.TOKEN_OPER, value:'NEG'},
+		{type:Calc.TOKEN_NUM, value:5},
+		{type:Calc.TOKEN_OPER, value:'*'},
+		{type:Calc.TOKEN_NUM, value:10},
+		{type:Calc.TOKEN_OPER, value:'NEG'},
+		{type:Calc.TOKEN_OPER, value:'/'},
+		{type:Calc.TOKEN_OPER, value:'+'},
+	];
+	calc.setRPN(rpn);
+	assert.deepEqual(calc.getRPN(), rpn);
+	assert.strictEqual(calc.calc(), -6.55);
+
+	rpn = [
+		{type:Calc.TOKEN_NUM, value:3},
+		{type:Calc.TOKEN_NUM, value:4},
+		{type:Calc.TOKEN_OPER, value:'NEG'},
+		{type:Calc.TOKEN_OPER, value:'-'},
+	];
+	calc.setRPN(rpn);
+	assert.deepEqual(calc.getRPN(), rpn);
+	assert.strictEqual(calc.calc(), 7);
+
+	rpn = [
+		{type:Calc.TOKEN_NUM, value:3},
+		{type:Calc.TOKEN_NUM, value:4},
+		{type:Calc.TOKEN_OPER, value:'NEG'},
+		{type:Calc.TOKEN_OPER, value:'-'},
+		{type:Calc.TOKEN_NUM, value:4},
+		{type:Calc.TOKEN_OPER, value:'+'},
+	];
+	calc.setRPN(rpn);
+	assert.deepEqual(calc.getRPN(), rpn);
+	assert.strictEqual(calc.calc(), 11);
+});
+QUnit.test("Random Expression", function(assert) {
+	var calc = new Calc(), count = 100;
+	while (count--){
+		var expr = exprGen(20);
+		assert.strictEqual(calc.compile(expr).calc(), eval(expr), expr);
 	}
-}
+});
 
